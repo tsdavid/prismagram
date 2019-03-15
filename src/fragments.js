5c1d124802e461d51f9797e8d971b0bcda929bba
root <root@DESKTOP-BBTSTTF.localdomain>
2019-03-15 15:25:21 +0900

    #3.13 See Full Posts: KILL BUGS

--- src/fragments.js
+++ src/fragments.js
@@ -1,7 +1,9 @@
-export const COMMENT_FRAGMENT = `fragment CommentParts on Comment{
+export const COMMENT_FRAGMENT = `
+fragment CommentParts on Comment{
     id
       text
       user{
         username
       }
-}`;
+}
+`;
